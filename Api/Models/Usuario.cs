7fed7b07bbe39a3b8fc8e63f37bbff18b6472668
MauraoCB <jzmsmauro@hotmail.com>
2024-08-20 23:37:32 -0300

    Criacao do Projeto

---
+++ Api/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopDown_API.Models
+{
+    public class Usuario
+    {
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public string Funcao { get; set; }
+    }
+}
