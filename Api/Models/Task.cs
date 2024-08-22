50e40e0b6c8530510786f0b5e218085638247d23
MauraoCB <jzmsmauro@hotmail.com>
2024-08-22 19:45:38 -0300

    Incluida a validacao

--- Api/Models/Task.cs
+++ Api/Models/Task.cs
@@ -6,7 +6,9 @@ namespace TopDown_API.Models
     public class Task
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Por favor informe o titulo")]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Por favor informe descricao")]
         public string Description { get; set; }
         public DateTime? DueDate { get; set; }
         public bool IsCompleted { get; set; }
